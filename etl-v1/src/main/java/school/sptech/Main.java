d8b69e04193ca733bee0d70ae6eb431520c76986
Davissantan4 <davi.ssilva@sptech.school>
2025-10-15 09:47:17 -0300

    Comentando a ETL

--- etl-v1/src/main/java/school/sptech/Main.java
+++ etl-v1/src/main/java/school/sptech/Main.java
@@ -1,5 +1,23 @@
 package school.sptech;
 
+// !!!IMPORTANTE!!! - LEIA ANTES DE EXECUTAR:
+/* Para esta primeira versão da ETL, o código está sendo executado localmente (considerando que não esteja rodando em uma EC2).
+    Dito isso, é necessário que você verifique sua conexão com a AWS para que o código seja executado sem problemas, então abra o
+    terminal do seu computador e use o comando aws configure para atualizar suas credenciais de acesso.
+    O script utiliza essas credenciais para se conectar aos buckets, então sem credenciais não há como acessar.
+
+    Ponto importante: A seleção dos buckets (para downloads e uploads) está sendo feita de forma dinâmica, mas tem um porém.
+    Isso foi feito assumindo que a ordem de criação dos buckets seja a seguinte: 1.raw 2.trusted 3.client (ou 0, 1, 2 respectivamente)
+
+    Para resumir o funcionamento do script: Acessa a AWS a partir das suas credenciais e baixa os arquivos do bucket raw,
+    em seguida trata os dados, envia para o bucket trusted e exclui os downloads feitos no computador.
+    O tratamento do trusted para o client está em aguardo, pois depende da tela de dashboards. Mas já adianto que, uma vez
+    que os dados a serem exibidos na tela estejam definidos, será preciso adicionar uma função nova no script referente a este tratamento
+    (que pode ser feito rapidamente).
+ */
+
+
+// 1. Importando as dependências necessárias
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
@@ -18,6 +36,7 @@ import org.apache.commons.csv.*;
 public class Main {
     public static void main(String[] args) {
 
+        // Estabelecendo conexão com a AWS
         S3Client s3Client = S3Client.builder().region(Region.US_EAST_1).build();
 
         baixarArquivos(s3Client);
@@ -26,17 +45,24 @@ public class Main {
 
     }
 
+    // 2. Função que baixa os arquivos do bucket
     public static void baixarArquivos(S3Client s3Client) {
         System.out.println("Baixando os arquivos do bucket...");
         try {
+            // Lista todos os buckets do usuário e guarda em uma lista, em seguida seleciona o primeiro bucket
+            // (índice 0) para fazer o download dos arquivos
             ListBucketsResponse response = s3Client.listBuckets();
             List<Bucket> bucketList = response.buckets();
-            ListObjectsV2Request req = ListObjectsV2Request.builder().bucket(bucketList.get(0).name()).build();
+            ListObjectsV2Request req = ListObjectsV2Request.builder().bucket(bucketList.getFirst().name()).build();
             ListObjectsV2Response res = s3Client.listObjectsV2(req);
 
+
+            // Cria um diretório localmente chamado "arquivos". O diretório irá guardar os arquivos baixados.
             Path pasta = Paths.get("arquivos");
             Files.createDirectories(pasta);
 
+
+            // Lista todos os arquivos do bucket e faz o download dentro da pasta arquivos.
             for (S3Object arquivo : res.contents()) {
 
                 Path destino = pasta.resolve(arquivo.key());
@@ -69,12 +95,14 @@ public class Main {
         System.out.println("Arquivos baixados com sucesso");
     }
 
+    // 3. Função para o tratamento dos arquivos baixados
     public static void tratarDados() {
         System.out.println("Iniciando tratamento dos dados...");
 
         File pasta = new File("arquivos");
         File[] arquivos = pasta.listFiles();
 
+        // Cria um novo diretório chamado "arquivos_tratados" para conter a modificação dos arquivos
         try {
             Path pastaTratada = Paths.get("arquivos_tratados");
             Files.createDirectories(pastaTratada);
@@ -85,6 +113,8 @@ public class Main {
             System.exit(1);
         }
 
+        // Lista todos os arquivos na pasta arquivos e os modifica, enviando a nova versão para o diretório arquivos_tratados
+        // Remove o diretório arquivos no final
         if (arquivos != null) {
             for (File arquivo : arquivos) {
                 try {
@@ -95,8 +125,10 @@ public class Main {
                     String line;
                     List<Log> logs = new ArrayList<>();
 
+                    // Ignora a primeira linha do arquivo (ou seja, o header)
                     reader.readLine();
 
+                    // Guarda os dados do arquivo em uma lista e cria um objeto Log com os dados
                     while ((line = reader.readLine()) != null) {
                         String[] dados = line.split(",");
 
@@ -114,6 +146,7 @@ public class Main {
                         logs.add(log);
                     }
 
+                    // Formata os dados e escreve em um novo arquivo
                     try {
                         writer.append("TIMESTAMP;UUID;ARRITMIA;CPU;RAM;DISCO;BATERIA;QTD_TAREFAS;TAREFAS\n");
 
@@ -161,6 +194,7 @@ public class Main {
 
     }
 
+    // 4. Função para remover os diretórios
     public static void removerPasta(String nomePasta) {
         System.out.println("Excluindo diretório " + nomePasta + "...");
 
@@ -178,9 +212,11 @@ public class Main {
         System.out.println("Diretório excluído com sucesso");
     }
 
+    // 5. Função para enviar os arquivos para um bucket
     public static void enviarArquivos(S3Client s3Client) {
         System.out.println("Iniciando envio dos arquivos...");
 
+        // Lista todos os arquivos do diretório arquivos_tratados e guarda em uma lista
         S3TransferManager transferManager = S3TransferManager.builder().build();
         File pasta = new File("arquivos_tratados");
         File[] arquivos = pasta.listFiles();
@@ -188,6 +224,7 @@ public class Main {
         ListBucketsResponse response = s3Client.listBuckets();
         List<Bucket> bucketList = response.buckets();
 
+        // Seleciona os arquivos na lista e envia para o bucket trusted
         if (arquivos != null) {
             for (File arquivo : arquivos) {
                 UploadFileRequest uploadFileRequest = UploadFileRequest.builder()
@@ -204,6 +241,8 @@ public class Main {
             System.out.println("Diretório não encontrado");
         }
 
+
+        // Exclui o diretório arquivos_tratados após o envio
         System.out.println("Envio de arquivos concluído com sucesso");
         removerPasta("arquivos_tratados");
 
